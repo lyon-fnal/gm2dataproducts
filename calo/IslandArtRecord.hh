9f13ad36045117075a2d9bad15216fff5c4cff10
Loreto Alonzi <alonzi@uw.edu>
2013-11-04 12:29:47 -0600

    added the header file for the data product

---
+++ calo/IslandArtRecord.hh
@@ -0,0 +1,50 @@
+#ifndef IslandArtRecord_hh
+#define IslandArtRecord_hh
+
+/** @file IslandArtRecord.hh
+ 
+    Implements the digitized samples in art record
+ 
+    @author loreto "pete" alonzi - alonzi@uw.edu
+    @date 2013
+
+ */
+
+#include <vector>
+
+namespace gm2dataproducts {
+  struct IslandArtRecord {
+    
+    /** Which calorimeter was hit. */
+    int caloNum;
+    
+    /** Which XTAL was hit. */
+    int xtalNum;
+    
+    /** Index of clock tick for sample */
+    int time;
+    
+    /** voltage vector for the island. length is variable
+	but we restrict it to be some multiple (n) of
+	the characteristic pulse length (L).*/
+    std::vector<short int> voltage;
+    
+    IslandArtRecord() :
+      caloNum(0), xtalNum(0), time(0)
+    {}
+    
+    virtual ~IslandArtRecord(){};
+    
+    // ROOT doesn't need to know the rest
+#ifndef __GCCXML__
+    IslandArtRecord(int cn, int xn, int time,std::vector<short int> v) :
+      caloNum(cn), xtalNum(xn), time(time), voltage(v)
+    {}
+#endif // __GCCXML__
+    
+  }; //end of IslandArtRecord struct
+  
+  typedef std::vector<IslandArtRecord> IslandArtRecordCollection;
+} // end namespace gm2dataproducts
+
+#endif // IslandArtRecord_hh
